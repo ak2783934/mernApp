bd04c6f52e77a8695f2614bca7513d96cdcac3aa
ak2783934 <ak2783934@gmail.com>
2020-12-11 21:28:50 +0530

    with the branch withphoto

--- client/src/App.js
+++ client/src/App.js
@@ -77,7 +77,7 @@ class App extends React.Component {
     console.log("State is :", this.state);
     return (
       <div className="app">
-        <h2>Upload your dick/pussies and confess the horny moment</h2>
+        <h2>Upload your thoughts</h2>
         <form onSubmit={this.submit}>
           <div className="form-input">
             <input
